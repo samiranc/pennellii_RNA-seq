bee8394cd2f8f85efca7d3b73682243cbdc8bb23
Samira Nassi Celaya <nassi.samira20@gmail.com>
2021-01-16 20:09:18 -0700

    # Downloaded Cedar's counts data

--- Rscript.R
+++ Rscript.R
@@ -4,11 +4,13 @@
 # path variable where imported counts.txt would be found
 # file.path(getwd() function is way to make code transferable to different operating systems
 
-path <- file.path(getwd(C:/Users/RAFAEL/Documents/pennellii_RNA-seq/R/data/counts.txt), "data", "counts.txt")
+path <- file.path(getwd(), "data", "counts.txt")
 
 # Actual import 
 ?read.table()
 # sep, and escape sequence, to insert horizontal tab when tab key is pressed 
 # header function is used if the first line of the file should be used for column names 
 
-counts <- read.table(C:\Users\RAFAEL\Documents\R\pennellii_RNA-seq\data\counts.txt = path, sep = '\t', header = FALSE)
+counts <- read.table(file = path, sep = '\t', header = FALSE)
+
+quit(save = yes)
